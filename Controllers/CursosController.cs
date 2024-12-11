f8ee82364d7113232349c8569c6d0009eec03db5
Jhonatan Torres <jtorress@isise.edu.pe>
2024-12-10 23:28:45 -0500

    Primer Commit

---
+++ Controllers/CursosController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationExamParcial.Models;
+
+namespace WebApplicationExamParcial.Controllers
+{
+    public class CursosController : Controller
+    {
+        // GET: Cursos
+        public ActionResult Index()
+        {
+            using (BD_ColegioEntities contexto = new BD_ColegioEntities())
+            {
+                return View(contexto.Cursos.ToList());
+            }
+        }
+
+        // GET: Cursos/Details/5
+        public ActionResult Details(int id)
+        {
+            using (BD_ColegioEntities contexto = new BD_ColegioEntities())
+            {
+                
+                var curso = contexto.Cursos.FirstOrDefault(x => x.Id == id);
+
+                if (curso == null)
+                {
+                    return HttpNotFound(); 
+                }
+
+                return View(curso);
+            }
+        }
+
+        // GET: Cursos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cursos/Create
+        [HttpPost]
+        public ActionResult Create(Cursos curso)
+        {
+            try
+            {
+                using (BD_ColegioEntities contexto = new BD_ColegioEntities())
+                {
+                    
+                    contexto.Cursos.Add(curso);
+                    contexto.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Cursos/Edit/5
+        public ActionResult Edit(int id)
+        {
+            using (BD_ColegioEntities contexto = new BD_ColegioEntities())
+            {
+                var curso = contexto.Cursos.Where(x => x.Id == id).FirstOrDefault();
+
+                if (curso == null)
+                {
+                    return HttpNotFound();  
+                }
+
+                return View(curso);
+            }
+        }
+
+        // POST: Cursos/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Cursos curso)
+        {
+            try
+            {
+                using (BD_ColegioEntities contexto = new BD_ColegioEntities())
+                {
+                    
+                    var cursoExistente = contexto.Cursos.Where(x => x.Id == id).FirstOrDefault();
+
+                    if (cursoExistente != null)
+                    {
+                        
+                        cursoExistente.Nombre_del_curso = curso.Nombre_del_curso;
+                        cursoExistente.Nivel = curso.Nivel;
+                        cursoExistente.Profesor = curso.Profesor;
+                        contexto.SaveChanges();
+                    }
+
+                    return RedirectToAction("Index");
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Cursos/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (BD_ColegioEntities contexto = new BD_ColegioEntities())
+            {
+                var curso = contexto.Cursos.Where(x => x.Id == id).FirstOrDefault();
+
+                if (curso == null)
+                {
+                    return HttpNotFound();  
+                }
+
+                return View(curso);
+            }
+        }
+
+        // POST: Cursos/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                using (BD_ColegioEntities contexto = new BD_ColegioEntities())
+                {
+                    var curso = contexto.Cursos.Where(x => x.Id == id).FirstOrDefault();
+
+                    if (curso != null)
+                    {
+                        contexto.Cursos.Remove(curso);
+                        contexto.SaveChanges();
+                    }
+
+                    return RedirectToAction("Index");
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
+
+
---
+++ Controllers/EstudiantesController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationExamParcial.Controllers
+{
+    using System;
+    using System.Linq;
+    using System.Web.Mvc;
+    using WebApplicationExamParcial.Models;  
+
+    public class EstudiantesController : Controller
+    {
+        // GET: Estudiantes
+        public ActionResult Index()
+        {
+            using (BD_ColegioEntities contexto = new BD_ColegioEntities())
+            {
+                
+                var estudiantes = contexto.Estudiantes.ToList();
+                return View(estudiantes); 
+            }
+        }
+
+        // GET: Estudiantes/Details/5
+        public ActionResult Details(int id)
+        {
+            using (BD_ColegioEntities contexto = new BD_ColegioEntities())
+            {
+                
+                var estudiante = contexto.Estudiantes.FirstOrDefault(x => x.Id == id);
+
+                if (estudiante == null)
+                {
+                    return HttpNotFound();  
+                }
+
+                return View(estudiante);  
+            }
+        }
+
+        // GET: Estudiantes/Create
+        public ActionResult Create()
+        {
+            return View();  
+        }
+
+        // POST: Estudiantes/Create
+        [HttpPost]
+        public ActionResult Create(Estudiantes estudiante)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (BD_ColegioEntities contexto = new BD_ColegioEntities())
+                    {
+                       
+                        contexto.Estudiantes.Add(estudiante);
+                        contexto.SaveChanges();  
+                    }
+
+                    return RedirectToAction("Index");  
+                }
+
+                return View(estudiante);  
+            }
+            catch
+            {
+                return View();  
+            }
+        }
+
+        // GET: Estudiantes/Edit/5
+        public ActionResult Edit(int id)
+        {
+            using (BD_ColegioEntities contexto = new BD_ColegioEntities())
+            {
+                
+                var estudiante = contexto.Estudiantes.FirstOrDefault(x => x.Id == id);
+
+                if (estudiante == null)
+                {
+                    return HttpNotFound();  
+                }
+
+                return View(estudiante);  
+            }
+        }
+
+        // POST: Estudiantes/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Estudiantes estudiante)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (BD_ColegioEntities contexto = new BD_ColegioEntities())
+                    {
+                        
+                        var estudianteExistente = contexto.Estudiantes.FirstOrDefault(x => x.Id == id);
+
+                        if (estudianteExistente != null)
+                        {
+                           
+                            estudianteExistente.Nombre = estudiante.Nombre;
+                            estudianteExistente.Carrera = estudiante.Carrera;
+                            estudianteExistente.CorreoElectronico = estudiante.CorreoElectronico;
+
+                            
+                            contexto.SaveChanges();
+                        }
+                    }
+
+                    return RedirectToAction("Index");  
+                }
+
+                return View(estudiante);  
+            }
+            catch
+            {
+                return View();  
+            }
+        }
+
+        // GET: Estudiantes/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (BD_ColegioEntities contexto = new BD_ColegioEntities())
+            {
+                
+                var estudiante = contexto.Estudiantes.FirstOrDefault(x => x.Id == id);
+
+                if (estudiante == null)
+                {
+                    return HttpNotFound();  
+                }
+
+                return View(estudiante);  
+            }
+        }
+
+        // POST: Estudiantes/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                using (BD_ColegioEntities contexto = new BD_ColegioEntities())
+                {
+                   
+                    var estudiante = contexto.Estudiantes.FirstOrDefault(x => x.Id == id);
+
+                    if (estudiante != null)
+                    {
+                        
+                        contexto.Estudiantes.Remove(estudiante);
+                        contexto.SaveChanges();  
+                    }
+                }
+
+                return RedirectToAction("Index");  
+            }
+            catch
+            {
+                return View();  
+            }
+        }
+    }
+}
+
